d15d505168d1606e558fee1b8fb36483d07dbaaa
wangquan117 <994256768@qq.com>
2025-07-10 11:50:22 +0800

    Update Flow_Light.py

--- Flow_Light.py
+++ Flow_Light.py
@@ -12,19 +12,23 @@ for pin in led_pins:
     GPIO.setup(pin, GPIO.OUT)
 
 try:
-    while True:
+    loop_count = 0
+    max_loop = 2
+    while loop_count<max_loop:
         
         for pin in led_pins:
             GPIO.output(pin, GPIO.HIGH)  
             time.sleep(0.1)               
- #           GPIO.output(pin, GPIO.LOW)    
+
 
         
         for pin in reversed(led_pins):
- #           GPIO.output(pin, GPIO.HIGH) 
+
             GPIO.output(pin, GPIO.LOW) 
-            time.sleep(0.1)               
- #           GPIO.output(pin, GPIO.LOW)    
+            time.sleep(0.1)  
+        
+        loop_count += 1 
+ 
 
 except KeyboardInterrupt:
     print("over")
